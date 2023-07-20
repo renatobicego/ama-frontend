0ea8fc088eb132aff434f4f86aad8faab3c92691
renatobicego <macbookpro@MacBook-Pro-de-admin.local>
2023-07-20 14:50:14 -0300

    cambio

--- src/app/admin/Panel.jsx
+++ src/app/admin/Panel.jsx
@@ -16,11 +16,79 @@ import {
     ChevronRightIcon, 
     Cog6ToothIcon, 
     InboxIcon, 
+    NewspaperIcon, 
     PowerIcon, 
     PresentationChartBarIcon, 
     ShoppingBagIcon, 
-    UserCircleIcon } from "@heroicons/react/24/outline";
+    TrophyIcon, 
+    UserCircleIcon, 
+    UsersIcon} from "@heroicons/react/24/outline";
 import { useState } from "react";
+
+const listItems = [
+  {
+    header: {
+      title: 'Torneos',
+      icon: <PresentationChartBarIcon className="h-5 w-5" />
+    },
+    open: 1,
+    body: [
+      {
+        icon: <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />,
+        item: 'Inscripciones'
+      },
+      {
+        icon: <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />,
+        item: 'Gestionar Torneos'
+      },
+      {
+        icon: <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />,
+        item: 'Crear Torneo'
+      },
+    ]
+  },
+  {
+    header: {
+      title: 'Noticias',
+      icon: <NewspaperIcon className="h-5 w-5" />
+    },
+    open: 2,
+    body: [
+      {
+        icon: <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />,
+        item: 'Escribir Noticia'
+      },
+      {
+        icon: <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />,
+        item: 'Editar Noticia'
+      },
+      {
+        icon: <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />,
+        item: 'Crear Torneo'
+      },
+    ]
+  },
+  {
+    header: {
+      title: 'Clubes',
+      icon: <UsersIcon className="h-5 w-5" />
+    },
+    open: 3,
+    body: [
+      
+    ]
+  },
+  {
+    header: {
+      title: 'Campeones',
+      icon: <TrophyIcon className="h-5 w-5" />
+    },
+    open: 4,
+    body: [
+      
+    ]
+  },
+]
 
 const PanelAdmin = () => {
     const [open, setOpen] = useState(0)
@@ -37,96 +105,45 @@ const PanelAdmin = () => {
             </Typography>
           </div>
           <List>
-            <Accordion
-              open={open === 1}
+            {listItems.map((item, i) => 
+              <Accordion
+              open={open === item.open}
+              key={i}
               icon={
-                <ChevronDownIcon
-                  strokeWidth={2.5}
-                  className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
-                />
-              }
-            >
-              <ListItem className="p-0" selected={open === 1}>
-                <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-3">
-                  <ListItemPrefix>
-                    <PresentationChartBarIcon className="h-5 w-5" />
-                  </ListItemPrefix>
-                  <Typography color="blue-gray" className="mr-auto font-normal">
-                    Torneos
-                  </Typography>
-                </AccordionHeader>
-              </ListItem>
-              <AccordionBody className="py-1">
-                <List className="p-0">
-                  <ListItem>
+                  <ChevronDownIcon
+                    strokeWidth={2.5}
+                    className={`mx-auto h-4 w-4 transition-transform ${open === item.open ? "rotate-180" : ""}`}
+                  />
+                }
+              >
+                <ListItem className="p-0" selected={open === item.open}>
+                  <AccordionHeader onClick={() => handleOpen(item.open)} className="border-b-0 p-3">
                     <ListItemPrefix>
-                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                      {item.header.icon}
                     </ListItemPrefix>
-                    Inscripciones
-                  </ListItem>
-                  <ListItem>
-                    <ListItemPrefix>
-                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                    </ListItemPrefix>
-                    Gestionar Torneos
-                  </ListItem>
-                  <ListItem>
-                    <ListItemPrefix>
-                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                    </ListItemPrefix>
-                    Crear Torneo
-                  </ListItem>
-                </List>
-              </AccordionBody>
-            </Accordion>
-            <Accordion
-              open={open === 2}
-              icon={
-                <ChevronDownIcon
-                  strokeWidth={2.5}
-                  className={`mx-auto h-4 w-4 transition-transform ${open === 2 ? "rotate-180" : ""}`}
-                />
-              }
-            >
-              <ListItem className="p-0" selected={open === 2}>
-                <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3">
-                  <ListItemPrefix>
-                    <ShoppingBagIcon className="h-5 w-5" />
-                  </ListItemPrefix>
-                  <Typography color="blue-gray" className="mr-auto font-normal">
-                    Noticias
-                  </Typography>
-                </AccordionHeader>
-              </ListItem>
-              <AccordionBody className="py-1">
-                <List className="p-0">
-                  <ListItem>
-                    <ListItemPrefix>
-                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                    </ListItemPrefix>
-                    Escribir Noticia
-                  </ListItem>
-                  <ListItem>
-                    <ListItemPrefix>
-                      <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                    </ListItemPrefix>
-                    Editar Noticia
-                  </ListItem>
-                </List>
-              </AccordionBody>
-            </Accordion>
-            <ListItem>
-              <ListItemPrefix>
-                <InboxIcon className="h-5 w-5" />
-              </ListItemPrefix>
-              Clubes
-            </ListItem>
-            <ListItem>
-              <ListItemPrefix>
-                <UserCircleIcon className="h-5 w-5" />
-              </ListItemPrefix>
-              Campeones Nacionales
-            </ListItem>
+                    <Typography color="blue-gray" className="mr-auto font-normal">
+                      {item.header.title}
+                    </Typography>
+                  </AccordionHeader>
+                </ListItem>
+                  {item.body.length ?  
+                    <AccordionBody className="py-1">
+                      {item.body.map((bodyItem, k) => 
+                        
+                          <List key={k} className="p-0">
+                            <ListItem>
+                              <ListItemPrefix>
+                                {bodyItem.icon}
+                              </ListItemPrefix>
+                              {bodyItem.item}
+                            </ListItem>
+                          </List>
+                      )}
+                    </AccordionBody>
+                    : ''
+                  }
+              </Accordion>
+            )}
           </List>
         </Card>
       );
